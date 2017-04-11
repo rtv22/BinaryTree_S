4a2fe634210199d575b0a14fb1e7e3dc2be5b5ed
rtv22 <rtvcojigor@gmail.com>
2017-04-11 18:08:34 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -3,20 +3,6 @@
 
 SCENARIO ("init", "[init]")
 {
-  Tree<int> obj;
+  BinaryTree<int> obj;
   REQUIRE(obj.root_() == nullptr);
-  REQUIRE(obj.get_count() == 0);
-}
-SCENARIO("insert", "[insert]")
-{
-  Tree<int> obj;
-  obj.insert_node(3);
-  REQUIRE(obj.find_node(3, obj.root_())->data == 3);
-}
-SCENARIO("find_node", "[find_node]")
-{
-  Tree<int> obj;
-  test.insert_node(2);
-  REQUIRE(obj.find_node(2, obj.root_()) != nullptr);
-  REQUIRE(obj.find_node(2, obj.root_())->data == 4);
 }
