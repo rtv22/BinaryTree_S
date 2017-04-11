09163135ae2115866bcde66c369e3b59cf8d85b5
rtv22 <rtvcojigor@gmail.com>
2017-04-11 17:51:28 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,9 +1,22 @@
 #include <BinaryTree.hpp>
 #include <catch.hpp>
 
-SSCENARIO ("init", "[init]")
+SCENARIO ("init", "[init]")
 {
-  BinaryTree<int> obj;
-  REQUIRE(obj.root() == NULLPTR);
-  REQUIRE(obj.data() == 0);
+  Tree<int> obj;
+  REQUIRE(obj.root_() == nullptr);
+  REQUIRE(obj.get_count() == 0);
+}
+SCENARIO("insert", "[insert]")
+{
+  Tree<int> obj;
+  obj.insert_node(3);
+  REQUIRE(obj.find_node(3, obj.root_())->data == 3);
+}
+SCENARIO("find_node", "[find_node]")
+{
+  Tree<int> obj;
+  test.insert_node(2);
+  REQUIRE(obj.find_node(2, obj.root_()) != nullptr);
+  REQUIRE(obj.find_node(2, obj.root_())->data == 4);
 }
