4e6f22bb6f98fa31a597fcca3da235c3cbd4c42b
rtv22 <rtvcojigor@gmail.com>
2017-04-12 18:31:45 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -28,3 +28,16 @@ SCENARIO("get root", "[get root]")
   obj.insert_node(4);
   REQUIRE(obj.root_() != 0);
 }
+
+SCENARIO ("reading/writing", "[read/write]")
+{
+  Tree<int> test1;
+  test1.insert_node(4);
+  test1.insert_node(3);
+  test1.writing("file2.txt");
+  Tree<int> test2;
+  test2.reading("file2.txt");
+  REQUIRE(test2.find_node(4, test2.root_())!= nullptr);
+  REQUIRE(test2.find_node(3, test2.root_())!= nullptr);
+  REQUIRE(test1.get_count() == test2.get_count());
+}
