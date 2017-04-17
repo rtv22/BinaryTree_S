234e6931f51e4e45c8f7c48c6db8bfdbb044c084
rtv22 <rtvcojigor@gmail.com>
2017-04-17 17:20:37 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -13,7 +13,7 @@ SCENARIO ("output to cout")
   tree.insert_node(3);
   tree.writing("file1.txt");
   BinaryTree <int> tree_1;
-  tree_1.read("file2.txt");
+  tree_1.reading("file2.txt");
   REQUIRE(tree.find_node(3, tree_1.root_())!= nullptr);
 }
 
