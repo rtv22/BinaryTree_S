a4bd2e6998dbec3e66072ca4827cb75adeb74e3c
rtv22 <rtvcojigor@gmail.com>
2017-05-01 14:17:45 +0300

    Update BinaryTree.hpp

--- include/BinaryTree.hpp
+++ include/BinaryTree.hpp
@@ -6,6 +6,9 @@
 using namespace std;
 
 template <typename T>
+std::ostream& operator<<(std::ostream&, const BinarySearchTree<T>&);
+
+template <typename T>
 struct Node {
 	Node *left;
 	Node *right;
