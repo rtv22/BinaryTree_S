0b67ca1d7d89acab5180839f50e8fdca0488104f
rtv22 <rtvcojigor@gmail.com>
2017-05-01 14:48:46 +0300

    Update BinaryTree.hpp

--- include/BinaryTree.hpp
+++ include/BinaryTree.hpp
@@ -5,6 +5,12 @@
 
 using namespace std;
 
++template<typename T>
+ +class BinaryTree; 
+ +
+ +template<typename T>
+ +std::ostream& operator<<(ostream& ost, BinaryTree<T>& temp);
+
 template <typename T>
 struct Node {
 	Node *left;
