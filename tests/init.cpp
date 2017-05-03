3c2bec1bf5e6a8b33dbf3f27e83db2b9a35736bb
rtv22 <rtvcojigor@gmail.com>
2017-05-03 22:11:29 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -22,3 +22,10 @@ SCENARIO("find_node", "[find_node]")
     REQUIRE(obj.find_node(2, obj.root_()) != nullptr);
     REQUIRE(obj.find_node(2, obj.root_())->data == 2);
   }
+
+SCENARIO("insert", "[insert]")
+{
+  Tree<int> obj;
+  obj.insert_node(3);
+  REQUIRE(obj.find_node(3, obj.root_())->data == 3);
+}
