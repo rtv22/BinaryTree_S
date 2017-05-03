efe81275721f7d9984f239648eae6736a298e5f5
rtv22 <rtvcojigor@gmail.com>
2017-05-03 22:10:33 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,3 +6,19 @@ SCENARIO ("init", "[init]")
   BinaryTree<int> obj;
   REQUIRE(obj.root_() == nullptr);
 }
+
+SCENARIO ("output to cout", "<<")
+{
+  BinaryTree<int> tree;
+  tree.insert_node(3);
+  REQUIRE( std::cout << tree );
+}
+
+SCENARIO("find_node", "[find_node]")
+  {
+    BinaryTree<int> obj;
+    obj.insert_node(4);
+    obj.insert_node(2);
+    REQUIRE(obj.find_node(2, obj.root_()) != nullptr);
+    REQUIRE(obj.find_node(2, obj.root_())->data == 2);
+  }
