45c1ac8b2aa8b5d29d5c7c63fee88a19b752f71d
rtv22 <rtvcojigor@gmail.com>
2017-05-15 19:31:44 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -29,3 +29,15 @@ SCENARIO("insert", "[insert]")
   obj.insert_node(3);
   REQUIRE(obj.find_node(3, obj.root_())->data == 3);
 }
+
+SCENARIO("insert", "[insert]")
+{
+  BinaryTree<int> obj;
+  obj.insert_node(1);
+  obj.insert_node(2);
+  obj.insert_node(3);
+  obj.insert_node(4);
+  obj.insert_node(5);
+  tree.remove_element(4);
+  REQUIRE(obj.find_node(4, obj.root_()) != nullptr);
+}
