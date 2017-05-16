b65bbd4286f6cd2b6fe4c8667d82a86cb646842a
rtv22 <rtvcojigor@gmail.com>
2017-05-17 01:23:27 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -36,7 +36,7 @@ SCENARIO("removeElement", "[remEl]")
   obj.insert_node(1);
   obj.insert_node(2);
   obj.insert_node(3);
-  obj.remove_element(1);
+  obj.deleteVal(1);
   REQUIRE(obj.find_node(1, obj.root_())== nullptr);
   REQUIRE(obj.find_node(2, obj.root_())== obj.root_());
   REQUIRE(obj.root_() != nullptr);
